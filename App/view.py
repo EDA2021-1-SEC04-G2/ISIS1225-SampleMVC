2f1d15fe13d38ad33c9ce1aee1df7794a6f7616d
Gregorio2701 <g.salazar@uniandes.edu.co>
2021-02-03 17:00:10 -0500

    vista

--- App/view.py
+++ App/view.py
@@ -42,7 +42,7 @@ def printMenu():
     print("Opciones:")
     print("1- Cargar Libros")
     print("2- Cargar Tags")
-    # TO-DO: Modificaciones para completar el laboratorio 1.
+    print("3-Cargar Identificadores de libros")
     print("0- Salir")
 
 
@@ -75,8 +75,8 @@ while True:
         print("Cargando información de tags....")
         tags = loadTags()
         print('Total de tags cargados: ' + str(lt.size(tags)))
-    # TO-DO: Modificaciones para completar el laboratorio 1.
-
+    elif int(inputs[0]) == 3:
+        print("Cargando informacion de identificadores....")
     else:
         sys.exit(0)
 sys.exit(0)
