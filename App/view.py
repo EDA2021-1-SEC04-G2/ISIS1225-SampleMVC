359e736167c0f9a8dd6a6dfaea03755ccf0018d8
Gregorio2701 <g.salazar@uniandes.edu.co>
2021-02-03 17:47:02 -0500

    Entrega Final – laboratorio 1

--- App/view.py
+++ App/view.py
@@ -59,6 +59,12 @@ def loadTags():
     """
     return controller.loadTags('GoodReads/tags.csv')
 
+def loadBooksTags():
+    """
+    Carga los BookTags
+    """
+    return controller.loadBooksTags('GoodReads/book_tags-small.csv')
+
 
 """
 Menu principal
@@ -77,6 +83,8 @@ while True:
         print('Total de tags cargados: ' + str(lt.size(tags)))
     elif int(inputs[0]) == 3:
         print("Cargando informacion de identificadores...")
+        book_tags= loadBooksTags()
+        print("Total de identificadores cargados: ")
     else:
         sys.exit(0)
 sys.exit(0)
